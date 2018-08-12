011a429ffa76d52b3cfdf398bd625650174acbd3
shing100 <shing100@naver.com>
2018-08-12 16:29:07 +0900

    엄청난 query 와 data 가져오기

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Movieql-client
 
 - yarn add react-router-dom apollo-boost react-apollo graphql-tag graphql --save
+- yarn add react-helmet
+
+
+## graphql 신택스 사용방법
+- https://github.com/gajus/babel-plugin-graphql-tag
+- https://github.com/apollographql/graphql-tag
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { Query } from "react-apollo";
+import { HOME_PAGE } from "./queries";
 
-const Home = () => <div>home</div>
+const Home = () => <Query query={HOME_PAGE}>
+                    {({loading, data, error}) => {
+                        if(loading) return "loading";
+                        if(error) return "Something happend";
+                        
+                    return data.movies.map(movie => 
+                        <h2 key={movie.id}>
+                            {movie.title} / {movie.rating} 
+                        </h2>
+                        )
+                    }}
+                    </Query>;
 
 export default Home;
